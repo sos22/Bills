ee4598bcb2188407d74915a70ff7e4d54e80a370
Steven Smith <sos22@octomonkey.org.uk>
2010-01-18 10:20:19 +0000

    Make the mkdb.sh script actually work.

--- mkdb.sh
+++ mkdb.sh
@@ -5,17 +5,16 @@ set -e
 mkdir -p local
 
 sq() {
+    echo "$*"
     echo "$*" | sqlite3 local/bills.db 
 }
 
-#sq "create table users(username primary key not null);"
-#sq "create table bills(billident INTEGER primary key, date TEXT, description TEXT);"
-#sq "create table charges(bill REFERENCES bills (billident), chargeident INTEGER primary key autoincrement, user REFERENCES users(username), amount REAL);"
+sq "create table users(username primary key not null);"
+sq "create table bills(billident INTEGER primary key, date TEXT, description TEXT, owner TEXT REFERENCES users (username) NOT NULL);"
+sq "create table charges(bill REFERENCES bills (billident), chargeident INTEGER primary key autoincrement, user REFERENCES users(username), amount REAL);"
 
-#sq "alter table users add column password;"
+sq "alter table users add column password;"
 
-#sq "alter table users add column is_admin;"
-
-#sq "alter table bills add column owner REFERENCES users (username) NOT NULL;"
+sq "alter table users add column is_admin;"
 
 sq "create table bill_attachments(attach_ident INTEGER PRIMARY KEY AUTOINCREMENT, bill_ident REFERENCES bills(billident), content BLOB, filename);"
